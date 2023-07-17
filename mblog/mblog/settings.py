5974e3940f2b60903be79d066774e67bf5c5d00c
jjsaku <jjsaku123@gmail.com>
2023-07-17 22:37:17 +0800

    7 mysql gcp

--- mblog/mblog/settings.py
+++ mblog/mblog/settings.py
@@ -73,16 +73,47 @@ TEMPLATES = [
 WSGI_APPLICATION = "mblog.wsgi.application"
 
 
-# Database
+# Database sqlite3
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+'''
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+'''
+'''
+# Database mysql
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "ch07www",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
+        "OPTIONS": {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
+    }
+}
+'''
+
+
+# Database google cloud
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "ch07www",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "34.80.106.201",
+        "PORT": "",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
